6a65e8056325400d019587b51dea1b351555fec0
davidcrc <david_drcr@hotmail.com>
2018-10-18 11:21:11 -0500

    Video 6: Marcadores y sus Eventos

--- app/src/main/java/com/mobiles/mapsperu/MapsActivity.java
+++ app/src/main/java/com/mobiles/mapsperu/MapsActivity.java
@@ -2,6 +2,8 @@ package com.mobiles.mapsperu;
 
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -9,11 +11,16 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+//public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, GoogleMap.OnMarkerClickListener {
 
     private GoogleMap mMap;
+
+    // Crear un marcador desde cero
+    private Marker markerPrueba;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,10 +48,38 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         // Add a marker in Sydney and move the camera - Latitud, longitud
         LatLng aqp = new LatLng(-16.39871566652775, -71.53667298251604);
-        mMap.addMarker(new MarkerOptions().position(aqp).title("Arequipa - Peru").snippet("Cruce ndependencia con paucarpata").icon(BitmapDescriptorFactory.fromResource(R.drawable.arequipa)));
+        mMap.addMarker(new MarkerOptions().position(aqp).title("Arequipa - Peru").snippet("Plaza de armas de arequipa!!").icon(BitmapDescriptorFactory.fromResource(R.drawable.arequipa)));
+
+        LatLng parqueSelva = new LatLng(-16.39007867124903, -71.53099669378967);
+        mMap.addMarker(new MarkerOptions().position(parqueSelva).draggable(true).title("Arequipa - Peru").snippet("Parque selva alegre").icon(BitmapDescriptorFactory.fromResource(R.drawable.peru)));
+
+        //Marcador de prueba
+        LatLng prueba = new LatLng(-16.40487652036424,-71.52662374006134);
+        markerPrueba = googleMap.addMarker(new MarkerOptions()
+                .position(prueba)
+                .draggable(true)        // para arrastrar el marcador
+                .title("Prueba")
+        );
 
         // Posicionar la camara , con un zoom
         //mMap.moveCamera(CameraUpdateFactory.newLatLng(aqp));
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(aqp, 14));
+
+        // Habilitar para escuchar eventos de los marcadores
+        googleMap.setOnMarkerClickListener(this);
+    }
+
+    @Override
+    public boolean onMarkerClick(Marker marker) {
+
+        if(marker.equals(markerPrueba)){
+            //Obtener la latitud y longitud desde el marcador
+            String lat, lng;
+            lat = Double.toString(marker.getPosition().latitude);
+            lng = Double.toString(marker.getPosition().longitude);
+
+            Toast.makeText(this, lat+", "+lng, Toast.LENGTH_SHORT).show();
+        }
+        return false;
     }
 }
